d900c5a94e1f09ecdcd7878ddaccf51074e71a37
Sára Ahmad <saraahmad0404@gmail.com>
2025-07-06 23:02:44 +0200

    adds

---
+++ src/main/java/com/example/JsonExample.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class JsonExample {
+    public static void main(String[] args) {
+        // 1. Objekt erstellen
+        Person p = new Person("Lilo", 100);
+
+        // 2. Gson benutzen, um das Objekt in JSON zu verwandeln
+        Gson gson = new Gson();
+        String json = gson.toJson(p);
+
+        // 3. Ausgabe
+        System.out.println(json);
+    }
+}
+
+// Hilfsklasse für die Person
+class Person {
+    String name;
+    int age;
+
+    // Konstruktor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testGreeting() {
+        assertEquals(5, 2 + 3); // richtiger Test, sollte grün sein
+    }
+}
